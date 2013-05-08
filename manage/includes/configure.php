cb460c9e51a79a4b76e20274fe071a13bb08a74b
jason <jason@cartstore.com>
2013-05-08 14:09:41 -0700

    database changes

--- manage/includes/configure.php
+++ manage/includes/configure.php
@@ -1,15 +1,13 @@
 <?php
-
-  
-  define('HTTP_SERVER', 'http://localhost');
-  define('HTTP_CATALOG_SERVER', 'http://localhost');
-  define('HTTPS_CATALOG_SERVER', 'http://localhost');
+  define('HTTP_SERVER', 'http://dev.cartstore.com');
+  define('HTTP_CATALOG_SERVER', 'http://dev.cartstore.com');
+  define('HTTPS_CATALOG_SERVER', 'http://dev.cartstore.com');
   define('ENABLE_SSL_CATALOG', 'false');
-  define('DIR_FS_DOCUMENT_ROOT', '/var/www/dev.cartstore.com/');
-  define('DIR_WS_ADMIN', '/dev.cartstore.com/manage/');
-  define('DIR_FS_ADMIN', '/var/www/dev.cartstore.com/manage/');
-  define('DIR_WS_CATALOG', '/dev.cartstore.com/');
-  define('DIR_FS_CATALOG', '/var/www/dev.cartstore.com/');
+  define('DIR_FS_DOCUMENT_ROOT', '/home/devcarts/public_html/');
+  define('DIR_WS_ADMIN', '/manage/');
+  define('DIR_FS_ADMIN', '/home/devcarts/public_html/manage/');
+  define('DIR_WS_CATALOG', '/');
+  define('DIR_FS_CATALOG', '/home/devcarts/public_html/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -23,49 +21,26 @@
   define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
   define('DIR_FS_CATALOG_IMAGES', DIR_FS_CATALOG . 'images/');
   define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
-  
   define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
-   
   define('DIR_FS_MAIL_DUMPS', DIR_FS_ADMIN . 'maildumps/');
- 
-  
-  
-  
 
   define('DB_SERVER', 'localhost');
-  
-  define('DB_SERVER_USERNAME', 'root');
-  
-  define('DB_SERVER_PASSWORD', '75gtyfh3');
-  
-  define('DB_DATABASE', 'devcartstorecom');
-  
+  define('DB_SERVER_USERNAME', 'devcarts_dbase');
+  define('DB_SERVER_PASSWORD', '2b^ovkuT9K}q');
+  define('DB_DATABASE', 'devcarts_79');
   define('USE_PCONNECT', 'false');
-  
   define('STORE_SESSIONS', 'mysql');
   
   define('PRODUCTS_OPTIONS_TYPE_TEXT', 1);
-  
   define('PRODUCTS_OPTIONS_TYPE_RADIO', 2);
-  
   define('PRODUCTS_OPTIONS_TYPE_CHECKBOX', 3);
-  
   define('PRODUCTS_OPTIONS_TYPE_FILE', 4);
-  
-  define('PRODUCTS_OPTIONS_TYPE_TEXTAREA', 5); 
-  
+  define('PRODUCTS_OPTIONS_TYPE_TEXTAREA', 5);
   define('PRODUCTS_OPTIONS_TYPE_CALENDER', 6);
-   
-   define('SR_ENABLED', false);
-   
-   define('SR_APITOKEN', "");
-   
-   define('SR_APISECRET', "");
-   
-   define('SR_ENDPOINT', "http://platform.social-runner.com/API/");
-   
-   
-   
-   
-   
+  define('DIR_WS_TEMPLATES', DIR_FS_CATALOG . 'templates/');
+  define('SR_ENABLED', true);
+  define('SR_APITOKEN', "CSpKz0XrhEWLDzwql5KsTA");
+  define('SR_APISECRET', "KfF_e0Ar_E2F7b4QgHDglw");
+  define('SR_ENDPOINT', "http://platform.social-runner.com/API/");
+
 ?>
